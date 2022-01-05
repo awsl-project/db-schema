b34ff2c5493504cde310669cbcac3dfc5a7e78d1
jinmu333 <dreamhunter2333@gmail.com>
2022-01-05 20:05:06 +0800

    feat: init remove db name

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 mysqldump command
 
 ```bash
-mysqldump -hlocalhost -uroot -proot --databases awsl --no-data --skip-comments | sed 's/ AUTO_INCREMENT=[0-9]*//g' > awsl.sql
+mysqldump -hlocalhost -uroot -proot --databases awsl --no-data --skip-comments --no-create-db | sed 's/ AUTO_INCREMENT=[0-9]*//g' | sed 's/USE [.*?]*[;]*//g' > awsl.sql
 ```
--- awsl.sql
+++ awsl.sql
@@ -10,9 +10,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `awsl` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
-USE `awsl`;
+`awsl`;
 DROP TABLE IF EXISTS `awsl_mblog`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
