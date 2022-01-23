2ffff1e59db59f4ddec85f04dc72dd992dfdb34b
jinmu333 <dreamhunter2333@gmail.com>
2022-01-23 22:58:00 +0800

    feat: add awsl_blob

--- awsl.sql
+++ awsl.sql
@@ -11,17 +11,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
+DROP TABLE IF EXISTS `awsl_blob`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `awsl_blob` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `awsl_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `pic_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pic_info` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pic_id` (`pic_id`),
+  KEY `awsl_id` (`awsl_id`),
+  KEY `awsl_blob_pic_id_IDX` (`pic_id`) USING BTREE,
+  CONSTRAINT `awsl_blob_ibfk_1` FOREIGN KEY (`awsl_id`) REFERENCES `awsl_mblog` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `awsl_mblog`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `awsl_mblog` (
-  `id` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `uid` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `mblogid` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `re_id` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `re_mblogid` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `re_user_id` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `re_user` text COLLATE utf8mb4_general_ci,
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `uid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `mblogid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `re_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `re_mblogid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `re_user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `re_user` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -30,10 +45,10 @@ DROP TABLE IF EXISTS `awsl_pic`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `awsl_pic` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `awsl_id` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `awsl_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `sequence` int DEFAULT NULL,
-  `pic_id` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `pic_info` text COLLATE utf8mb4_general_ci,
+  `pic_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `pic_info` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`id`),
   KEY `awsl_id` (`awsl_id`),
   CONSTRAINT `awsl_pic_ibfk_1` FOREIGN KEY (`awsl_id`) REFERENCES `awsl_mblog` (`id`)
@@ -44,8 +59,8 @@ DROP TABLE IF EXISTS `awsl_producer`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `awsl_producer` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `uid` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `uid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `profile` json DEFAULT NULL,
   `keyword` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `max_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
